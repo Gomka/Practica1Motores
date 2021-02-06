bd09b0dd618e86e3bad45562dcafd9a31014ea62
Gonzalo Marcos Vaquero <55784961+Gomka@users.noreply.github.com>
2021-02-06 11:47:01 +0100

    porky

--- ClickerGame/main/clickCounter.lua
+++ ClickerGame/main/clickCounter.lua
@@ -2,19 +2,14 @@ local Counter = {}
 Counter.clicks = 0
 Counter.mouseClicks = 0
 
-function Counter.mouseClick()
+function Counter.click()
 	Counter.clicks = Counter.clicks + 1
 	Counter.mouseClicks = Counter.mouseClicks + 1
 
 	if Counter.mouseClicks >= 10 then
 		Counter.mouseClicks = 0
-		print("ping")
 		msg.post("main:/boarFactory#boarFactory", "createBoar")
 	end
-end
-
-function Counter.boarClick()
-	Counter.clicks = Counter.clicks + 1
 end
 
 function Counter.getClicks()
