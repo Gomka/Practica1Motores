4e89d48ac5d8165591712190be116dc4dff110f4
Ryuggha <rogerperalesmares493@gmail.com>
2021-02-07 19:08:01 +0100

    Terminated

--- ClickerGame/main/clickCounter.lua
+++ ClickerGame/main/clickCounter.lua
@@ -1,15 +1,24 @@
 local Counter = {}
 Counter.clicks = 0
 Counter.mouseClicks = 0
+Counter.numClicksToBoar = 10
 
-function Counter.click()
-	Counter.clicks = Counter.clicks + 1
-	Counter.mouseClicks = Counter.mouseClicks + 1
+function Counter.click(value)
+	Counter.clicks = Counter.clicks + value
+	Counter.mouseClicks = Counter.mouseClicks + value
 
-	if Counter.mouseClicks >= 10 then
+	if Counter.mouseClicks >= Counter.numClicksToBoar then
 		Counter.mouseClicks = 0
 		msg.post("main:/boarFactory#boarFactory", "createBoar")
 	end
+end
+
+function Counter.getMouseClicks()
+	return Counter.mouseClicks
+end
+
+function Counter.getNumClicksToBoar()
+	return Counter.numClicksToBoar
 end
 
 function Counter.getClicks()
